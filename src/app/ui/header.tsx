ee9352e748dd74b9718515b1e590ae8c85f27c54
harveycoombs <harveycoombs23@outlook.com>
2024-09-20 15:18:12 +0100

    popups

--- src/app/ui/header.tsx
+++ src/app/ui/header.tsx
@@ -1,19 +1,39 @@
+"use client";
+import { useState } from "react";
 import Link from "next/link";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faClockRotateLeft, faBug } from "@fortawesome/free-solid-svg-icons";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
+import Popup from "./popup";
+
 export default function Header() {
+    let [historyIsVisible, setHistoryVisibility] = useState(false);
+
+    function openHistory() {
+        setHistoryVisibility(true);
+    }
+    
+    function closeHistory() {
+        setHistoryVisibility(false);
+    }
+
     return (
-        <header className="absolute top-0 left-0 right-0 flex justify-between items-center p-4">
-            <strong className="text-sm font-bold">WRITTEN IN <span className="text-slate-800">RUST</span> BY <a href="https://harveycoombs.com/" target="_blank" className="text-slate-800 decoration-2 hover:underline">HARVEY COOMBS</a></strong>
-            <div className="text-sm font-bold pointer-events-none select-none">UPLOADS OLDER THAN 30 DAYS ARE DELETED &middot; 5GB MAXIMUM UPLOAD SIZE</div>
-            <nav>
-                <HeaderNavigationItem title="View Upload History" icon={faClockRotateLeft} />
-                <HeaderNavigationItem url="mailto:contact@harveycoombs.com" title="Report an Issue" icon={faBug} />
-                <HeaderNavigationItem url="https://github.com/harveycoombs/share" title="View on GitHub" icon={faGithub} />
-            </nav>
-        </header>
+        <>
+            <header className="absolute top-0 left-0 right-0 flex justify-between items-center p-4">
+                <strong className="text-sm font-bold">WRITTEN IN <span className="text-slate-800">RUST</span> BY <a href="https://harveycoombs.com/" target="_blank" className="text-slate-800 decoration-2 hover:underline">HARVEY COOMBS</a></strong>
+                <div className="text-sm font-bold pointer-events-none select-none">UPLOADS OLDER THAN 30 DAYS ARE DELETED &middot; 5GB MAXIMUM UPLOAD SIZE</div>
+                <nav>
+                    <HeaderNavigationItem title="View Upload History" icon={faClockRotateLeft} click={openHistory} />
+                    <HeaderNavigationItem url="mailto:contact@harveycoombs.com" title="Report an Issue" icon={faBug} />
+                    <HeaderNavigationItem url="https://github.com/harveycoombs/share" title="View on GitHub" icon={faGithub} />
+                </nav>
+            </header>{
+            historyIsVisible && (
+                <Popup title="Upload History" close={closeHistory} />
+            )}
+        </>
     );
 }
 
@@ -21,6 +41,6 @@ function HeaderNavigationItem(props: any) {
     let classes = "inline-block align-middle text-xl ml-4 duration-150 cursor-pointer hover:text-slate-400 active:text-slate-500";
 
     return (
-        props.url?.length ? <Link href={props.url} target="_blank" className={classes} title={props.title} draggable="false"><FontAwesomeIcon icon={props.icon} /></Link> : <div className={classes} title={props.title} draggable="false"><FontAwesomeIcon icon={props.icon} /></div>
+        props.url?.length ? <Link href={props.url} target="_blank" className={classes} title={props.title} draggable="false"><FontAwesomeIcon icon={props.icon} /></Link> : <div className={classes} title={props.title} draggable="false" onClick={props.click}><FontAwesomeIcon icon={props.icon} /></div>
     );
 }
---
+++ src/app/ui/popup.tsx
@@ -0,0 +1,15 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
+
+export default function Popup(props: any) {
+    return (
+        <div className="fixed inset-0 grid place-items-center bg-slate-800 bg-opacity-80">
+            <div className="p-3 bg-slate-50 rounded min-w-96">
+                <div className="flex justify-between items-center text-sm">
+                    <strong className="font-extrabold text-slate-800">{props.title.toUpperCase()}</strong>
+                    <div className="text-lg cursor-pointer duration-150 hover:text-red-500" onClick={props.close}><FontAwesomeIcon icon={faXmark} /></div>
+                </div>
+            </div>{props.content ?? ""}
+        </div>
+    );
+}
