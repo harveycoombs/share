c7826ea2f7e45eafcd391aa419704342725781f0
harveycoombs <harveycoombs23@outlook.com>
2024-09-20 12:57:05 +0100

    more

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,15 @@
+import Button from "./ui/button";
+
 export default function Home() {
   return (
-    <main></main>
+    <main className="grid place-items-center h-screen">
+      <section className="text-center">
+          <h1 className="text-5xl font-black text-slate-800 pointer-events-none duration-150">DROP FILES ONTO THIS PAGE</h1>
+          <div className="w-fit mt-8 mb-0 ml-auto mr-auto">
+              <strong className="inline-block align-middle text-xl font-extrabold mr-4 pointer-events-none">OR</strong>
+              <Button text="Browse Files" classes={["inline-block", "align-middle"]} />
+          </div>
+      </section>
+    </main>
   );
 }
--- src/app/ui/header.tsx
+++ src/app/ui/header.tsx
@@ -3,18 +3,17 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faClockRotateLeft, faBug } from "@fortawesome/free-solid-svg-icons";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
-export default function Header(props: any) {
+export default function Header() {
     return (
-        <header className="flex justify-between items-center p-4">
+        <header className="absolute top-0 left-0 right-0 flex justify-between items-center p-4">
             <strong className="text-sm font-bold">WRITTEN IN <span className="text-slate-800">RUST</span> BY <a href="https://harveycoombs.com/" target="_blank" className="text-slate-800 decoration-2 hover:underline">HARVEY COOMBS</a></strong>
-            <div className="text-sm font-bold">UPLOADS OLDER THAN 30 DAYS ARE DELETED &middot; 5GB MAXIMUM UPLOAD SIZE</div>
+            <div className="text-sm font-bold pointer-events-none select-none">UPLOADS OLDER THAN 30 DAYS ARE DELETED &middot; 5GB MAXIMUM UPLOAD SIZE</div>
             <nav>
                 <HeaderNavigationItem title="View Upload History" icon={faClockRotateLeft} />
                 <HeaderNavigationItem url="mailto:contact@harveycoombs.com" title="Report an Issue" icon={faBug} />
                 <HeaderNavigationItem url="https://github.com/harveycoombs/share" title="View on GitHub" icon={faGithub} />
             </nav>
         </header>
-
     );
 }
 
