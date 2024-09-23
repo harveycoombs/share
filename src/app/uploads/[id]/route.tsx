bf37dfeb3deea647128589146d80b84eeec3d5f0
harveycoombs <harveycoombs23@outlook.com>
2024-09-23 22:59:59 +0100

    upload file serving

---
+++ src/app/uploads/[id]/route.tsx
@@ -0,0 +1,33 @@
+import { NextResponse } from "next/server";
+import fs from "fs/promises";
+import mime from "mime";
+
+export async function GET(_: any, { params }: any) {
+    let { id } = params;
+
+    let files: string[] = [];
+
+    try {
+        files = await fs.readdir(`./uploads/${id}`);
+    } catch {
+        return NextResponse.json({ error: "The specified upload does not exist." }, { status: 404 });
+    }
+
+    files = files.filter(file => file != "." && file != "..");
+
+    switch (files.length) {
+        case 0:
+            return NextResponse.json({ error: "The specified upload does not exist." }, { status: 404 });
+        case 1:
+            let content = await fs.readFile(`./uploads/${id}/${files[0]}`);
+
+            return new NextResponse(content, {
+                headers: {
+                    "Content-Type": mime.getType(`./uploads/${id}/${files[0]}`) ?? "application/octet-stream",
+                    "Content-Disposition": `attachment; filename="${files[0]}"`,
+                }
+            });
+        default:
+            return;
+    }
+  }
