7878fd68d088798fe95a21f994d786159babe56a
harveycoombs <harveycoombs23@outlook.com>
2024-09-24 13:15:11 +0100

    release banner

--- src/app/ui/header.tsx
+++ src/app/ui/header.tsx
@@ -1,9 +1,9 @@
 "use client";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import Link from "next/link";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faClockRotateLeft, faBug, faFlag } from "@fortawesome/free-solid-svg-icons";
+import { faClockRotateLeft, faBug, faFlag, faXmark } from "@fortawesome/free-solid-svg-icons";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 import Popup from "./popup";
@@ -75,6 +75,7 @@ export default function Header() {
 
     return (
         <>
+            <Banner text="&#127881; Share 3.0.0 is here. Check out whats changed by clicking here." />
             <header className="absolute top-0 left-0 right-0 flex justify-between items-center p-4">
                 <strong className="text-sm font-bold">MADE WITH <span className="text-slate-800">REACT</span> BY <a href="https://harveycoombs.com/" target="_blank" className="text-slate-800 decoration-2 hover:underline">HARVEY COOMBS</a></strong>
                 <div className="text-sm font-bold pointer-events-none select-none">UPLOADS OLDER THAN 30 DAYS ARE DELETED &middot; 5GB MAXIMUM UPLOAD SIZE</div>
@@ -96,5 +97,18 @@ function HeaderNavigationItem(props: any) {
 
     return (
         props.url?.length ? <Link href={props.url} target="_blank" className={classes} title={props.title} draggable="false"><FontAwesomeIcon icon={props.icon} /></Link> : <div className={classes} title={props.title} draggable="false" onClick={props.click}><FontAwesomeIcon icon={props.icon} /></div>
+    );
+}
+
+function Banner(props: any) {
+    let banner = useRef<HTMLDivElement>(null);
+
+    function closeBanner() {
+        if (!banner?.current) return;
+        banner.current.remove();
+    }
+
+    return (
+        <div className="relative p-1.5 text-sm font-medium text-center bg-blue-100 text-blue-600" ref={banner}>{props.text}<div className="absolute right-3 top-0 translate-y-px text-lg cursor-pointer hover:text-blue-400" onClick={closeBanner}><FontAwesomeIcon icon={faXmark} /></div></div>
     );
 }
