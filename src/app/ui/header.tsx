5d57d34dabcb53cb3c6ecd25458ff534e6da6106
harveycoombs <harveycoombs23@outlook.com>
2024-09-27 12:25:16 +0100

    banner update

--- src/app/ui/header.tsx
+++ src/app/ui/header.tsx
@@ -99,7 +99,7 @@ export default function Header() {
     return (
         <>
             <header className="absolute top-0 left-0 right-0">
-                <Banner><span>&#127881; Share 3.0.0 is here. Check out whats changed by clicking <a href="https://github.com/harveycoombs/share/releases" target="_blank" className="hover:underline">here</a>.</span></Banner>
+                <Banner><span>&#127881; Share 3.1.3 is here. Check out whats changed by clicking <a href="https://github.com/harveycoombs/share/releases" target="_blank" className="hover:underline">here</a>.</span></Banner>
                 <div className="flex justify-between items-center p-4 max-[460px]:p-3">
                     <strong className="text-sm font-bold max-lg:text-xs">MADE <span className="max-[460px]:hidden">WITH <span className="text-slate-800 dark:text-zinc-400">REACT</span></span> BY <a href="https://harveycoombs.com/" target="_blank" className="text-slate-800 decoration-2 hover:underline dark:text-zinc-400">HARVEY COOMBS</a></strong>
                     <div className="text-sm font-bold pointer-events-none select-none max-lg:hidden">UPLOADS OLDER THAN 30 DAYS ARE DELETED &middot; 5GB MAXIMUM UPLOAD SIZE</div>
