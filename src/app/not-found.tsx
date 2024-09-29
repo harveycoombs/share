f0aafc2dd6fc90d22ec10b8d90d071d88ac31db6
harveycoombs <harveycoombs23@outlook.com>
2024-09-29 16:16:29 +0100

    Added custom 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,12 @@
+import Button from "./ui/button";
+
+export default function NotFoundPage() {
+    return (
+        <main className="h-full grid place-items-center">
+            <div>
+                <h1 className="text-5xl font-black text-amber-400 px-5 pointer-events-none select-none max-lg:text-4xl max-[460px]:hidden dark:text-zinc-500">404 &middot; NOT FOUND</h1>
+                <Button url="/" classes="block w-fit mx-auto mt-6">TAKE ME HOME</Button>
+            </div>
+        </main>
+    );
+}
