46918b1d4afff5c79044a32f2439dee36d1eaf49
Harvey Coombs <70911622+harveycoombs@users.noreply.github.com>
2024-10-02 10:55:13 +0100

    Removal of disclaimer link from footer

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
             <footer className="absolute bottom-0 left-0 right-0">
                 <div className="flex justify-between items-center p-4 max-[460px]:p-3">
                     <strong className="text-sm font-bold max-lg:text-xs">MADE <span className="max-[460px]:hidden">WITH <span className="text-slate-800 dark:text-zinc-400">REACT</span></span> BY <Link href="https://harveycoombs.com/" target="_blank" className="text-slate-800 decoration-2 hover:underline dark:text-zinc-400">HARVEY COOMBS</Link></strong>
-                    <strong className="text-sm font-bold max-lg:text-xs"><Link href="/disclaimer.txt" className="decoration-2 hover:underline" target="_blank">DISCLAIMER</Link> &middot; <Link href="/takedowns" className="decoration-2 hover:underline">TAKEDOWN REQUESTS</Link></strong>
+                    <Link href="/takedowns" className="decoration-2 text-sm font-bold max-lg:text-xs hover:underline">TAKEDOWN REQUESTS</Link>
                 </div>
             </footer>
     );
