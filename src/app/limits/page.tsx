dccc9d5409fe8612eb180e92a92a8daed3ecad74
harveycoombs <harveycoombs23@outlook.com>
2024-11-16 02:13:39 +0000

    Added other pages

---
+++ src/app/limits/page.tsx
@@ -0,0 +1,8 @@
+export default function Limits() {
+    return (
+        <div className="flex flex-col items-center justify-center h-screen">
+            <h1 className="text-6xl font-semibold duration-150 pointer-events-none select-none">Increase Upload Limits</h1>
+            <h2 className="text-xl font-semibold my-8 pointer-events-none select-none">Coming Soon</h2>
+        </div>
+    );
+}
