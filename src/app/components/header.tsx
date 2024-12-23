9ec5a1f363140fec21aa1ddf38ac848cc9d8d51b
harveycoombs <harveycoombs23@outlook.com>
2024-12-23 23:33:23 +0000

    Rebrand (2)

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -4,11 +4,15 @@ import Link from "next/link";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
+import Logo from "@/app/components/common/logo";
+
 export default function Header() {
     return (
         <header className="absolute top-0 left-0 right-0 select-none">
             <div className="flex justify-between items-center p-5 max-[460px]:p-3">
-                <Link href="/" className="font-bold select-none leading-none duration-150 hover:opacity-70">Share <span className="text-slate-400/60">{process.env.APP_VERSION}</span></Link>
+                <Link href="/" className="font-bold select-none leading-none duration-150 hover:opacity-70">
+                    <Logo width={126} height={24} />
+                </Link>
                 <nav className="flex items-center gap-8 flex-nowrap">
                     <HeaderLink url="https://github.com/harveycoombs/share/issues/new" text="Report Issue" />
                     <HeaderLink url="/limits" text="Increase Limit" />
