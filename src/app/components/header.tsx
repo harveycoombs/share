300d72f9dc05f57189c51a13cf0894c4174e0d43
harveycoombs <harveycoombs23@outlook.com>
2024-12-25 23:25:26 +0000

    Header

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { useState, useEffect } from "react";
 import Image from "next/image";
 import Link from "next/link";
@@ -5,6 +6,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import AccountSettings from "@/app/components/common/popups/account";
 import LoginForm from "@/app/components/common/popups/login";
+import { faRightToBracket, faUser } from "@fortawesome/free-solid-svg-icons";
 
 export default function Header() {
     let [user, setUser] = useState<any>(null);
@@ -28,13 +30,13 @@ export default function Header() {
 
     return (
         <>
-            <header className="p-3 flex justify-between items-center">
-                <Link href="/"><Image src="/images/logo.png" alt="Share.surf" width={28} height={28} /></Link>
+            <header className="p-4 flex justify-between items-center">
+                <Link href="/"><Image src="/images/icon.png" alt="Share.surf" width={28} height={28} /></Link>
                 <nav>
                     <HeaderLink title="About" url="/about" />
                     <HeaderLink title="Support" url="/support" />
                     <HeaderLink title="Premium" url="/premium" />
-                    {user ? <HeaderIcon icon="fa-solid fa-user" title={`Signed in as ${user.firstname} ${user.lastname}`} onClick={() => setAccountSettingsVisibility(true)} /> : <HeaderIcon icon="fa-solid fa-user" title="Sign in" onClick={() => setLoginFormVisibility(true)} />}
+                    {user ? <HeaderIcon icon={faUser} title={`Signed in as ${user.firstname} ${user.lastname}`} onClick={() => setAccountSettingsVisibility(true)} /> : <HeaderIcon icon={faRightToBracket} title="Sign in" onClick={() => setLoginFormVisibility(true)} />}
                 </nav>
             </header>
             {accountSettingsAreVisible && <AccountSettings user={user} onClose={() => setAccountSettingsVisibility(false)} />}
@@ -44,9 +46,9 @@ export default function Header() {
 }
 
 function HeaderLink({ title, url }: any) {
-    return <Link href={url} className="">{title}</Link>;
+    return <Link href={url} className="inline-block align-middle text-sm font-medium leading-none select-none mr-8 cursor-pointer duration-100 hover:text-slate-500 active:text-slate-400" draggable={false}>{title}</Link>;
 }
 
 function HeaderIcon({ icon, title, ...rest }: any) {
-    return <div title={title} className="" {...rest}><FontAwesomeIcon icon={icon} /></div>;
+    return <div title={title} className="inline-block align-middle text-lg text-slate-400/60 leading-none select-none translate-y-px cursor-pointer duration-100 hover:text-slate-400 active:text-slate-500/85" {...rest}><FontAwesomeIcon icon={icon} /></div>;
 }
