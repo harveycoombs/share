4fe3e03178e0a007db942521f652d162f8971637
harveycoombs <harveycoombs23@outlook.com>
2024-12-26 00:11:52 +0000

    home page setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+import Logo from "@/app/components/common/logo";
+
 export default function Home() {
     return (
-        <>
-            <section>a</section>
-        </>
+        <section>
+            <div>
+                <Logo width={288} height={56} />
+                <strong className="block font-medium text-slate-400 text-center mt-4">The no-frills file sharing service</strong>
+            </div>
+            <div>
+                <h1>Drop files onto this page to upload</h1>
+            </div>
+        </section>
     );
 }
