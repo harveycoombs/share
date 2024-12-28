6ee169c5797d40e1dd7a6ad5d975361d12106e06
harveycoombs <harveycoombs23@outlook.com>
2024-12-28 00:15:33 +0000

    text selection prevention

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import Link from "next/link";
 
 export default function Home() {
     return (
-        <section className="text-center w-600 mx-auto">
+        <section className="text-center w-600 mx-auto select-none">
             <div>
                 <div className="w-fit mx-auto"><Logo width={288} height={56} /></div>
                 <strong className="block font-medium text-slate-400 mt-4">The no-frills file sharing service</strong>
@@ -19,16 +19,16 @@ export default function Home() {
                     <Button classes="inline-block align-middle">Browse Files</Button>
                     <Button classes="inline-block align-middle ml-2" transparent={true}><FontAwesomeIcon icon={faClockRotateLeft} /> View Upload History</Button>
                 </div>
-                <div className="my-6 text-blue-500 text-center select-none">
+                <div className="my-6 text-blue-500 text-center">
                     <FontAwesomeIcon icon={faInfoCircle} className="inline-block align-middle text-lg leading-none" />
                     <span className="inline-block align-middle text-xs leading-none font-semibold ml-2">2GB Upload Limit</span>
                 </div>
             </div>
             <div className="flex gap-2.5 w-full p-2.5 rounded-lg bg-slate-100 text-slate-400">
-                <Image src="/images/collate-icon.png" alt="Collate AI" width={75} height={75} className="block rounded shrink-0 aspect-square" />
+                <Image src="/images/collate-icon.png" alt="Collate AI" width={75} height={75} className="block rounded shrink-0 aspect-square pointer-events-none" />
                 <div className="text-left">
                     <Link href="https://collate.run/" target="_blank" rel="noopener" className="block text-sm font-semibold text-slate-500 mb-1.5 hover:underline"><FontAwesomeIcon icon={faExternalLinkAlt} /> Collate: Find &amp; summarise anything on the web with AI</Link>
-                    <p className="text-[0.8rem] font-medium">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio tempore maxime facere est recusandae ullam necessitatibus doloremque minima laborum magni!</p>
+                    <p className="text-[0.8rem] font-medium pointer-events-none">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio tempore maxime facere est recusandae ullam necessitatibus doloremque minima laborum magni!</p>
                 </div>
             </div>
         </section>
