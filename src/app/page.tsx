38b161425769f2c9a12dde17976b152fe723fde2
harveycoombs <harveycoombs23@outlook.com>
2024-12-28 00:14:04 +0000

    collate ad adjustments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,11 +24,11 @@ export default function Home() {
                     <span className="inline-block align-middle text-xs leading-none font-semibold ml-2">2GB Upload Limit</span>
                 </div>
             </div>
-            <div className="flex gap-2.5 w-full p-2.5 rounded-lg bg-slate-200/60 text-slate-400">
+            <div className="flex gap-2.5 w-full p-2.5 rounded-lg bg-slate-100 text-slate-400">
                 <Image src="/images/collate-icon.png" alt="Collate AI" width={75} height={75} className="block rounded shrink-0 aspect-square" />
                 <div className="text-left">
                     <Link href="https://collate.run/" target="_blank" rel="noopener" className="block text-sm font-semibold text-slate-500 mb-1.5 hover:underline"><FontAwesomeIcon icon={faExternalLinkAlt} /> Collate: Find &amp; summarise anything on the web with AI</Link>
-                    <p className="text-[0.8rem]">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio tempore maxime facere est recusandae ullam necessitatibus doloremque minima laborum magni!</p>
+                    <p className="text-[0.8rem] font-medium">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio tempore maxime facere est recusandae ullam necessitatibus doloremque minima laborum magni!</p>
                 </div>
             </div>
         </section>
