f19d20f2ae39e8f01861599142aac07bbf204276
harveycoombs <harveycoombs23@outlook.com>
2025-01-03 23:09:52 +0000

    added focus style to field component + updated depdencies

--- src/app/components/common/field.tsx
+++ src/app/components/common/field.tsx
@@ -5,6 +5,6 @@ interface Properties {
 }
 
 export default function Field({ type, classes, ...rest }: Properties) {
-    let classList = `px-5 py-2.5 rounded-full text-[0.8rem] border border-slate-300 bg-transparent leading-none duration-150 ${classes?.length && " " + classes}`;
+    let classList = `px-5 py-2.5 rounded-full text-[0.8rem] border border-slate-300 bg-transparent leading-none duration-150 focus:outline-none focus:border-blue-500 ${classes?.length && " " + classes}`;
     return <input type={type ?? "text"} className={classList} {...rest} />;
 }
