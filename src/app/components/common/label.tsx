0ff44e43af23a8b44d90ad73e9b28eee6e00f87c
harveycoombs <harveycoombs23@outlook.com>
2025-01-03 20:49:26 +0000

    form appearance (2)

---
+++ src/app/components/common/label.tsx
@@ -0,0 +1,8 @@
+interface Properties {
+    children: React.ReactNode;
+    classes?: string;
+}
+
+export default function Label({ children, classes }: Properties) {
+    return <label className={`text-xs font-medium text-slate-400/70${classes?.length ? " " + classes : ""}`}>{children}</label>;
+}
--- src/app/components/common/popups/login.tsx
+++ src/app/components/common/popups/login.tsx
@@ -1,6 +1,9 @@
+import Link from "next/link";
+
 import Popup from "@/app/components/common/popup";
-import Button from "../button";
-import Field from "../field";
+import Button from "@/app/components/common/button";
+import Field from "@/app/components/common/field";
+import Label from "@/app/components/common/label";
 
 interface Properties {
     onClose: () => void;
@@ -9,9 +12,16 @@ interface Properties {
 export default function LoginForm({ onClose }: Properties) {
     return (
         <Popup title="Sign In" onClose={onClose}>
-            <form className="">
-                <Field type="email" placeholder="Email Address" classes="block w-full" />
-                <Button classes="block w-full">Continue</Button>
+            <form className="w-72">
+                <Label>Email Address</Label>
+                <Field type="email" classes="block w-full" />
+                <Label classes="mt-2.5">Password</Label>
+                <Field type="password" classes="block w-full" />
+                <Button classes="block w-full mt-2.5">Continue</Button>
+                <Button transparent={true} classes="block w-full mt-2.5">Register</Button>
+                <div>
+                    <Link href="/">Recover Account</Link> &middot; <Link href="/">Report Issue</Link>
+                </div>
             </form>
         </Popup>
     );
