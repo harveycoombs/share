489265fea8dd502c939bdc6d34ad56586ca1187d
Harvey Coombs <70911622+harveycoombs@users.noreply.github.com>
2025-01-14 23:48:20 +0000

    Added sidebar item component for account settings popup

--- src/app/components/common/popups/account.tsx
+++ src/app/components/common/popups/account.tsx
@@ -9,9 +9,13 @@ export default function AccountSettings({ userid, onClose }: Properties) {
     return (
         <Popup title="Account Settings" onClose={onClose}>
             <div className="flex gap-3 w-500">
-                <div className="w-28">a</div>
+                <div className="w-28"></div>
                 <div>b</div>
             </div>
         </Popup>
     );
+}
+
+function SidebarItem({ title, ...rest }: any) {
+    return <div className="px-2 py-1 rounded text-sm leading-none font-medium" {...rest}>{title}</div>;
 }
