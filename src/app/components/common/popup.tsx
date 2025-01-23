c559f0e4f2d54caf585ba476413d6a9c80fa42b5
Harvey Coombs <70911622+harveycoombs@users.noreply.github.com>
2025-01-23 13:43:31 +0000

    fields (1)

--- src/app/components/common/popup.tsx
+++ src/app/components/common/popup.tsx
@@ -10,7 +10,7 @@ interface Properties {
 export default function Popup({ title, onClose, children, ...rest }: Properties) {
     return (
         <div className="fixed inset-0 z-50 grid place-items-center bg-slate-900/60" id="popup" onMouseDown={(e: any) => {if (e.target.matches("#popup")) onClose() }}>
-            <div className="bg-white p-4 rounded-lg" {...rest}>
+            <div className="bg-white p-3 rounded-lg" {...rest}>
                 <div className="flex justify-between items-center leading-none pb-1.5">
                     <strong className="text-sm font-medium text-slate-400/60 select-none">{title}</strong>
                     <div className="text-slate-400/60 cursor-pointer duration-150 hover:text-slate-400 active:text-slate-500/85" onClick={onClose}><FontAwesomeIcon icon={faXmark} /></div>
--- src/app/components/common/popups/account.tsx
+++ src/app/components/common/popups/account.tsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 
 import Popup from "@/app/components/common/popup";
 import Field from "@/app/components/common/field";
+import Label from "@/app/components/common/label";
 
 interface Properties {
     onClose: () => void;
@@ -24,21 +25,40 @@ export default function AccountSettings({ onClose }: Properties) {
     const [sectionTitle, setSectionTitle] = useState<string>();
     const [sectionContent, setSectionContent] = useState<React.JSX.Element>();
 
+    const accountDetailsSection = <>
+        <div className="mt-2 w-full flex gap-2">
+            <div className="w-1/2">
+                <Label classes="block w-full">First Name</Label>
+                <Field small={true} classes="block w-full" />
+            </div>
+
+            <div className="w-1/2">
+                <Label classes="block w-full">Last Name</Label>
+                <Field small={true} classes="block w-full" />
+            </div>
+        </div>
+
+        <div className="mt-2 w-full flex gap-2">
+            <div className="w-1/2">
+                <Label classes="block w-full">Email Address</Label>
+                <Field type="email" small={true} classes="block w-full" />
+            </div>
+        </div>
+    </>;
+
     useEffect(() => {
         switch (currentSection) {
             case "general":
                 setSectionTitle("General Settings");
-                setSectionContent(<>
-                    <Field />
-                </>);
+                setSectionContent(<div>Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>);
                 break;
             case "account":
                 setSectionTitle("Account Details");
-                setSectionContent(<div>Dolor sit</div>);
+                setSectionContent(accountDetailsSection);
                 break;
             case "advanced":
                 setSectionTitle("Advanced Settings");
-                setSectionContent(<div>Amet consectetur</div>);
+                setSectionContent(<div>Lorem ipsum dolor sit amet.</div>);
                 break;
         }
     }, [currentSection]);
@@ -53,13 +73,13 @@ export default function AccountSettings({ onClose }: Properties) {
     return (
         <Popup title="Settings" onClose={onClose}>
             <div className="flex gap-3 w-500">
-                <div className="w-28">
+                <div className="w-28 shrink-0">
                     <SidebarItem title="General" selected={currentSection == "general"} onClick={() => setCurrentSection("general")} />
                     <SidebarItem title="Account" selected={currentSection == "account"} onClick={() => setCurrentSection("account")} />
                     <SidebarItem title="Advanced" selected={currentSection == "advanced"} onClick={() => setCurrentSection("advanced")} />
                     <div className="p-1.5 mb-1 rounded text-[0.8rem] leading-none text-red-500 font-medium duration-150 cursor-pointer select-none hover:bg-red-50 active:bg-red-100" onClick={logout}>Log Out</div>
                 </div>
-                <div>
+                <div className="w-full">
                     <strong className="block text-sm font-semibold select-none">{sectionTitle}</strong>
                     <div>{sectionContent}</div>
                 </div>
