e06ca6be303685c0adc192f2f9195be28d9fb72d
harveycoombs <harveycoombs23@outlook.com>
2025-04-10 21:09:59 +0100

    added adsense meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -47,7 +47,7 @@ export default function RootLayout({
             <head>
                 <link rel="apple-touch-icon" sizes="180x180" href="/images/icon.png" />
                 <link rel="canonical" href="https://share.surf" />
-                <script async src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.GOOGLE_ADSENSE_ID}`} crossOrigin="anonymous"></script>
+                <meta name="google-adsense-account" content="ca-pub-7873492896986281" />
             </head>
 
             <body className={`${inter.className} h-screen bg-white text-slate-800`}>
