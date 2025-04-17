703cb3743699470c9f64b191446509073553f601
Harvey Coombs <70911622+harveycoombs@users.noreply.github.com>
2025-04-17 10:43:29 +0100

    Removed unused data from stats API endpoint

--- app/api/stats/route.ts
+++ app/api/stats/route.ts
@@ -1,19 +1,17 @@
 import { NextResponse } from "next/server";
 
-import { getTotalUsers, getTotalVerifiedUsers, getTotalUnverifiedUsers, getTotalDeletedUsers, getOldestUser, getNewestUser, getTotalUploads, getTotalUploadsFromGuests, getTotalUploadsFromRegisteredUsers, getTotalUploadsStorageUsed } from "@/lib/users";
+import { getTotalUsers, getTotalVerifiedUsers, getTotalUnverifiedUsers, getTotalDeletedUsers, getTotalUploads, getTotalUploadsFromGuests, getTotalUploadsFromRegisteredUsers, getTotalUploadsStorageUsed } from "@/lib/users";
 
 export async function GET(): Promise<NextResponse> {
     const totalUsers = await getTotalUsers();
     const totalVerifiedUsers = await getTotalVerifiedUsers();
     const totalUnverifiedUsers = await getTotalUnverifiedUsers();
     const totalDeletedUsers = await getTotalDeletedUsers();
-    const oldestUser = await getOldestUser();
-    const newestUser = await getNewestUser();
 
     const totalUploads = await getTotalUploads();
     const totalUploadsFromGuests = await getTotalUploadsFromGuests();
     const totalUploadsFromRegisteredUsers = await getTotalUploadsFromRegisteredUsers();
     const storageUsed = await getTotalUploadsStorageUsed();
 
-    return NextResponse.json({ totalUsers, totalVerifiedUsers, totalUnverifiedUsers, totalDeletedUsers, oldestUser, newestUser, totalUploads, totalUploadsFromGuests, totalUploadsFromRegisteredUsers, storageUsed }, { status: 200 });
+    return NextResponse.json({ totalUsers, totalVerifiedUsers, totalUnverifiedUsers, totalDeletedUsers, totalUploads, totalUploadsFromGuests, totalUploadsFromRegisteredUsers, storageUsed }, { status: 200 });
 }
