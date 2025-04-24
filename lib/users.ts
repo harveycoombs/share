64411b1f11d85d70be657676e238fd101dbe2d1f
Harvey Coombs <70911622+harveycoombs@users.noreply.github.com>
2025-04-24 12:59:25 +0100

    Changed user IDs to UUIDs.

--- lib/users.ts
+++ lib/users.ts
@@ -2,7 +2,7 @@
 import pool from "./database";
 import * as Passwords from "./passwords";
 
-export async function getUserByID(userid: number): Promise<any> {
+export async function getUserByID(userid: string): Promise<any> {
 	const [result]: any = await pool.query("SELECT user_id, first_name, last_name FROM users WHERE user_id = ? AND deleted = 0", [userid]);
 	return result[0];
 }
@@ -12,12 +12,12 @@ export async function getUserByEmailAddress(emailAddress: string): Promise<any> 
 	return result[0];
 }
 
-export async function getUserDetails(userid: number): Promise<any> {
+export async function getUserDetails(userid: string): Promise<any> {
     const [result]: any = await pool.query("SELECT user_id, first_name, last_name, email_address, creation_date FROM users WHERE user_id = ? AND deleted = 0", [userid]);
     return result[0];
 }
 
-export async function getUserSettings(userid: number): Promise<any> {
+export async function getUserSettings(userid: string): Promise<any> {
     const [result]: any = await pool.query("SELECT * FROM settings WHERE user_id = ?", [userid]);
     return result[0];
 }
@@ -38,56 +38,56 @@ export async function verifyCredentials(emailAddress: string, password: string):
 	return valid;
 }
 
-export async function emailExists(emailAddress: string, userid: number = 0): Promise<boolean> {
+export async function emailExists(emailAddress: string, userid: string|null = null): Promise<boolean> {
 	const [result]: any = await pool.query("SELECT COUNT(*) AS total FROM users WHERE email_address = ? AND user_id <> ? AND deleted = 0", [emailAddress, userid]);
 	return result[0].total > 0;
 }
 
 export async function createUser(firstName: string, lastName: string, emailAddress: string, password: string): Promise<number> {
 	const passwordHash = await Passwords.generateHash(password);
-	const [result]: any = await pool.query("INSERT INTO users (creation_date, first_name, last_name, email_address, password) VALUES ((SELECT NOW()), ?, ?, ?, ?)", [firstName, lastName, emailAddress, passwordHash]);
+	const [result]: any = await pool.query("INSERT INTO users (user_id, creation_date, first_name, last_name, email_address, password) VALUES ((SELECT UUID()), (SELECT NOW()), ?, ?, ?, ?)", [firstName, lastName, emailAddress, passwordHash]);
 
 	return result?.insertId ?? 0;
 }
 
-export async function updateUser(userid: number, firstName: string, lastName: string, emailAddress: string): Promise<boolean> {
+export async function updateUser(userid: string, firstName: string, lastName: string, emailAddress: string): Promise<boolean> {
     const [result]: any = await pool.query("UPDATE users SET first_name = ?, last_name = ?, email_address = ? WHERE user_id = ?", [firstName, lastName, emailAddress, userid]);
     return result.affectedRows > 0;
 }
 
-export async function updateUserPassword(userid: number, password: string): Promise<boolean> {
+export async function updateUserPassword(userid: string, password: string): Promise<boolean> {
     const passwordHash = await Passwords.generateHash(password);
     const [result]: any = await pool.query("UPDATE users SET password = ? WHERE user_id = ?", [passwordHash, userid]);
 
     return result.affectedRows > 0;
 }
 
-export async function updateUserAuthCode(userid: number, code: number): Promise<boolean> {
+export async function updateUserAuthCode(userid: string, code: number): Promise<boolean> {
     const [result]: any = await pool.query("UPDATE users SET auth_code = ? WHERE user_id = ?", [code, userid]);
     return result.affectedRows > 0;
 }
 
-export async function deleteUser(userid: number): Promise<boolean> {
+export async function deleteUser(userid: string): Promise<boolean> {
     const [result]: any = await pool.query("UPDATE users SET deleted = 1 WHERE user_id = ?", [userid]);
     return result.affectedRows > 0;
 }
 
-export async function getUploadHistory(userid: number): Promise<any[]> {
+export async function getUploadHistory(userid: string): Promise<any[]> {
     const [result]: any = await pool.query("SELECT *, 0 AS available FROM uploads WHERE user_id = ? ORDER BY upload_date DESC", [userid]);
     return result;
 }
 
-export async function insertUploadHistory(userid: number, name: string, ip: string, files: number, size: number): Promise<boolean> {
+export async function insertUploadHistory(userid: string, name: string, ip: string, files: number, size: number): Promise<boolean> {
     const [result]: any = await pool.query("INSERT INTO uploads (user_id, name, ip_address, files, size, upload_date) VALUES (?, ?, ?, ?, ?, NOW())", [userid, name, ip, files, size]);
     return result.affectedRows > 0;
 }
 
-export async function deleteUpload(userid: number, id: number): Promise<boolean> {
+export async function deleteUpload(userid: string, id: number): Promise<boolean> {
     const [result]: any = await pool.query("DELETE FROM uploads WHERE user_id = ? AND upload_id = ?", [userid, id]);
     return result.affectedRows > 0;
 }
 
-export async function renameUpload(userid: number, id: number, name: string): Promise<boolean> {
+export async function renameUpload(userid: string, id: number, name: string): Promise<boolean> {
     const [result]: any = await pool.query("UPDATE uploads SET title = ? WHERE user_id = ? AND upload_id = ?", [name, userid, id]);
     return result.affectedRows > 0;
 }
