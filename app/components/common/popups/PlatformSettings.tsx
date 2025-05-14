b294ff36a285dbc081a251869c170b8098308cc9
Harvey Coombs <70911622+harveycoombs@users.noreply.github.com>
2025-05-14 12:38:43 +0100

    initial platform settings popup content

--- app/components/common/popups/PlatformSettings.tsx
+++ app/components/common/popups/PlatformSettings.tsx
@@ -1,4 +1,7 @@
-import Popup from "../Popup";
+import Popup from "@/app/components/common/Popup";
+import Label from "@/app/components/common/Label";
+import Menu from "@/app/components/common/Menu";
+import Button from "@/app/components/common/Button";
 
 interface Properties {
     onClose: () => void;
@@ -7,7 +10,16 @@ interface Properties {
 export default function PlatformSettings({ onClose }: Properties) {
     return (
         <Popup title="Settings" onClose={onClose}>
-            to-do
+            <div className="w-64">
+                <Label classes="block w-full mt-2.75 mb-0.5">Language</Label>
+                <Menu classes="block w-full">
+                    <option value="en">English (EN)</option>
+                    <option value="de">German (DE)</option>
+                    <option value="de">Italian (IT)</option>
+                </Menu>
+
+                <Button classes="block w-full mt-2.75">Save</Button>
+            </div>
         </Popup>
     );
 }
