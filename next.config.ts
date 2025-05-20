ab012686cfc0dfb830c4fb24572e1095789e863d
harveycoombs <harveycoombs23@outlook.com>
2025-05-20 20:10:11 +0100

    removed config options

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    images: {
-        unoptimized: false
-    },
-    productionBrowserSourceMaps: false
 };
 
 export default nextConfig;
