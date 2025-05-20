90684cd378ec6cf6296cb81a24e313d720fccd85
harveycoombs <harveycoombs23@outlook.com>
2025-05-20 20:08:25 +0100

    fixed logic for optional classes for button component

--- app/components/common/Button.tsx
+++ app/components/common/Button.tsx
@@ -15,9 +15,9 @@ interface Properties {
     [key: string]: any;
 }
 
-export default function Button({ children, url, small, classes, transparent, color = "bg-indigo-500 hover:bg-indigo-600 active:bg-indigo-700", loading, disabled, ...rest }: Properties) {
+export default function Button({ children, url, small, classes = "", transparent, color = "bg-indigo-500 hover:bg-indigo-600 active:bg-indigo-700", loading, disabled, ...rest }: Properties) {
     const appearance = transparent ? "font-semibold hover:bg-slate-50 hover:text-slate-500 active:bg-slate-100 active:text-slate-600 dark:hover:bg-zinc-800/60 dark:active:bg-zinc-800/90 dark:hover:text-zinc-400 dark:active:text-zinc-400" : `font-medium text-white ${color}`;
-    const classList = `px-5 py-3 rounded-md text-[0.8rem] leading-none ${appearance} duration-150 ${loading ? "" : "cursor-pointer"} text-center select-none${classes?.length && " " + classes} `;
+    const classList = `px-5 py-3 rounded-md text-[0.8rem] leading-none ${appearance} duration-150 ${loading ? "" : "cursor-pointer"} text-center select-none${classes.length > 0 && " " + classes} `;
 
     return url?.length ? <Link href={url} className={classList} {...rest}>{loading ? <FontAwesomeIcon icon={faCircleNotch} className="animate-spin" /> : children}</Link> : <button className={classList} disabled={disabled || loading} {...rest}>{loading ? <FontAwesomeIcon icon={faCircleNotch} className="animate-spin" /> : children}</button>;
 }
