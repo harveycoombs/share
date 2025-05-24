7b3e593cd71cdb05789fa9892b0d0cc36a998437
harveycoombs <harveycoombs23@outlook.com>
2025-05-24 23:56:27 +0100

    Added issue reporting form

--- app/api/issue/route.ts
+++ app/api/issue/route.ts
@@ -8,7 +8,7 @@ import { authenticate } from "@/lib/jwt";
 export async function POST(request: Request): Promise<NextResponse> {
     const data = await request.formData();
 
-    const content = data.get("content")?.toString() ?? "";
+    const description = data.get("description")?.toString() ?? "";
     const name = data.get("name")?.toString() ?? "";
     const emailAddress = data.get("email")?.toString() ?? "";
 
@@ -25,7 +25,7 @@ export async function POST(request: Request): Promise<NextResponse> {
         const emailParams = new EmailParams()
             .setFrom({ email: "noreply@share.surf", name: "Share.surf" })
             .setSubject("Share.surf - Issue")
-            .setHtml(`<p>${content}</p><br/><br/><p>Reporter Name: <strong>${name}</strong></p><p>Reporter Email Address: <strong>${emailAddress}</strong></p><p>User ID: <strong>${user?.user_id ?? "Guest"}</strong></p><p>IP Address: <strong>${ipAddress}</strong></p><p>User Agent: <strong>${userAgent}</strong></p>`);
+            .setHtml(`<p>${description}</p><br/><br/><p>Reporter Name: <strong>${name}</strong></p><p>Reporter Email Address: <strong>${emailAddress}</strong></p><p>User ID: <strong>${user?.user_id ?? "Guest"}</strong></p><p>IP Address: <strong>${ipAddress}</strong></p><p>User Agent: <strong>${userAgent}</strong></p>`);
     
         await sendEmail(emailParams, recipients);
 
--- app/components/common/popups/IssueForm.tsx
+++ app/components/common/popups/IssueForm.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import Popup from "@/app/components/common/Popup";
 import Label from "@/app/components/common/Label";
@@ -15,39 +15,63 @@ export default function IssueForm({ onClose }: Properties) {
     const [name, setName] = useState<string>("");
     const [email, setEmail] = useState<string>("");
     const [description, setDescription] = useState<string>("");
-    const [error, setError] = useState<boolean>(false);
+
+    const [loading, setLoading] = useState<boolean>(false);
+    const [success, setSuccess] = useState<boolean>(false);
+    const [countdown, setCountdown] = useState<number>(5);
 
     async function submit() {
-        if (!name || !email || !description) {
-            setError(true);
+        if (!name.length || !email.length || !description.length) {
+            setSuccess(false);
             return;
         }
 
-        setError(false);
+        setSuccess(false);
+        setLoading(true);
 
-        const response = await fetch("/api/issue");
+        const response = await fetch("/api/issue", {
+            method: "POST",
+            body: new URLSearchParams({ name, email, description })
+        });
+
         const json = await response.json();
 
+        setLoading(false);
+
         if (!response.ok || !json.success) {
-            setError(true);
+            setSuccess(false);
             return;
         }
 
-        onClose();
+        setSuccess(true);
     }
+
+    useEffect(() => {
+        if (success) {
+            const interval = setInterval(() => {
+                const newCountdown = countdown - 1;
+                setCountdown(newCountdown);
+
+                if (newCountdown <= 0) {
+                    clearInterval(interval);
+                    onClose();
+                }
+            }, 1000);
+        }
+    }, [success, countdown]);
 
     return (
         <Popup title="Report An Issue" classes="w-100" onClose={onClose}>
             <Label classes="block w-full mt-2.75">Your Name</Label>
-            <Field classes="block w-full" warning={error && !name.length} onInput={(e: any) => setName(e.target.value)} />
+            <Field classes="block w-full" warning={!success && !name.length} onInput={(e: any) => setName(e.target.value)} />
 
             <Label classes="block w-full mt-2.75">Your Email Address</Label>
-            <Field classes="block w-full" warning={error && !email.length} onInput={(e: any) => setEmail(e.target.value)} />
+            <Field classes="block w-full" warning={!success && !email.length} onInput={(e: any) => setEmail(e.target.value)} />
 
             <Label classes="block w-full mt-2.75">Describe the issue</Label>
-            <TextBox classes="block w-full max-h-50 min-h-10" warning={error && !description.length} onInput={(e: any) => setDescription(e.target.value)} rows={8} />
+            <TextBox classes="block w-full max-h-50 min-h-10" warning={!success && !description.length} onInput={(e: any) => setDescription(e.target.value)} rows={8} />
 
-            <Button classes="block w-full mt-2.75" onClick={submit}>Submit</Button>
+            {success ? <Button classes="block w-full mt-2.75" color="bg-green-500 hover:bg-green-600 active:bg-green-700" onClick={onClose}>Submitted. Closing in {countdown}s</Button> : <Button classes="block w-full mt-2.75" onClick={submit} loading={loading}>Submit</Button>}
         </Popup>
     );
 }
