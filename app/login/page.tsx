19d39df4789a6697f4e74c2b8bae45991b494c65
Harvey Coombs <70911622+harveycoombs@users.noreply.github.com>
2025-06-19 12:35:49 +0100

    Added padding and height corrections to the registration, login & stats pages for smaller screens.

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -10,8 +10,8 @@ export default function Login() {
     const [issueFormVisibility, setIssueFormVisibility] = useState<boolean>(false);
 
     return (
-        <main className="h-[calc(100vh-52px)] grid place-items-center">
-            <section className="w-68">
+        <main className="min-h-[calc(100vh-52px)] grid place-items-center">
+            <section className="w-68 py-3.5">
                 <Link href="/" className="block w-fit mx-auto mb-5 select-none cursor-pointer duration-150 hover:opacity-80 active:opacity-60"><Logo width={220} height={43} /></Link>
 
                 <strong className="block font-semibold text-lg text-center mt-2 select-none">Welcome Back</strong>
