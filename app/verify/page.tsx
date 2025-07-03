c746c68932c0aa8902ee57ae164e709bc17d605a
Harvey Coombs <70911622+harveycoombs@users.noreply.github.com>
2025-07-03 12:40:39 +0100

    Verification form appearance

--- app/verify/page.tsx
+++ app/verify/page.tsx
@@ -1,129 +1,28 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Link from "next/link";
+
+import VerificationForm from "./form";
+import Logo from "@/app/components/common/Logo";
 
 export default function Verify() {
-    const [verifying, setVerifying] = useState<boolean>(false);
+    const [email, setEmail] = useState<string>("");
 
-    const [firstDigit, setFirstDigit] = useState<string>("");
-    const [secondDigit, setSecondDigit] = useState<string>("");
-    const [thirdDigit, setThirdDigit] = useState<string>("");
-    const [fourthDigit, setFourthDigit] = useState<string>("");
-    const [fifthDigit, setFifthDigit] = useState<string>("");
-    const [sixthDigit, setSixthDigit] = useState<string>("");
-
-    async function verify() {
-        if (!firstDigit?.length || !secondDigit?.length || !thirdDigit?.length || !fourthDigit?.length || !fifthDigit?.length || !sixthDigit?.length) {
-            setFeedback(<div className="text-sm font-medium text-amber-500 text-center mt-5">Please enter all digits</div>);
-            return;
-        }
-
-        const code = `${firstDigit}${secondDigit}${thirdDigit}${fourthDigit}${fifthDigit}${sixthDigit}`;
-
-        setLoading(true);
-
-        const response = await fetch("/api/user/verify", {
-            method: "POST",
-            body: new URLSearchParams({ email: emailAddress, code })
-        });
-
-        const json = await response.json();
-
-        switch (response.status) {
-            case 200:
-                if (!json.success) break;
-                window.location.href = "/";
-                break;
-            default:
-                setFeedback(<div className="text-sm font-medium text-red-500 text-center mt-5">Something went wrong</div>);
-                setErrorExistence(true);
-                break;
-        }
-
-        setLoading(false);
-    }
-
-    function handleDigitInput(e: any, index: number) {
-        const value = e.target.value;
-
-        switch (index) {
-            case 0:
-                setFirstDigit(value);
-                break;
-            case 1:
-                setSecondDigit(value);
-                break;
-            case 2:
-                setThirdDigit(value);
-                break;
-            case 3:
-                setFourthDigit(value);
-                break;
-            case 4:
-                setFifthDigit(value);
-                break;
-            case 5:
-                setSixthDigit(value);
-                break;
-        }
-
-        if (!value?.length) {
-            e.target.previousSibling?.focus();
-
-            switch (index) {
-                case 0:
-                    setFirstDigit("");
-                    break;
-                case 1:
-                    setSecondDigit("");
-                    break;
-                case 2:
-                    setThirdDigit("");
-                    break;
-                case 3:
-                    setFourthDigit("");
-                    break;
-                case 4:
-                    setFifthDigit("");
-                    break;
-                case 5:
-                    setSixthDigit("");
-                    break;
-            }
-        } else {
-            e.target.nextSibling?.focus();
-        }
-    }
-
-    function handlePaste(e: any) {
-        if (!e.clipboardData?.getData("text")?.length) return;
-
-        const text = (e.clipboardData?.getData("text") ?? "");
-
-        setFirstDigit(text.charAt(0));
-        setSecondDigit(text.charAt(1));
-        setThirdDigit(text.charAt(2));
-        setFourthDigit(text.charAt(3));
-        setFifthDigit(text.charAt(4));
-        setSixthDigit(text.charAt(5));
-
-        e.preventDefault();
-    }
+    useEffect(() => {
+        let parameter = new URLSearchParams(window.location.search)?.get("email") ?? "";
+        setEmail(decodeURI(parameter).trim());
+    }, []);
 
     return (
-        <form onSubmit={verify} onInput={() => { setFeedback(null); setErrorExistence(false); setWarningExistence(false); }}>
-            {feedback}
-            <Label classes="block mt-5" error={errorExists} warning={warningExists}>Verification Code</Label>
+        <main className="min-h-[calc(100vh-117px)] grid place-items-center">
+            <section className="w-75.5 py-3.5">
+                <Link href="/" className="block w-fit mx-auto mb-5 select-none cursor-pointer duration-150 hover:opacity-80 active:opacity-60"><Logo width={220} height={43} /></Link>
 
-            <div className="w-full mt-2.5 grid grid-cols-6 gap-2">
-                <Field error={errorExists} warning={warningExists} classes="text-center" defaultValue={firstDigit} onInput={(e: any) => handleDigitInput(e, 0)} onPaste={handlePaste} />
-                <Field error={errorExists} warning={warningExists} classes="text-center" defaultValue={secondDigit} onInput={(e: any) => handleDigitInput(e, 1)} onPaste={handlePaste} />
-                <Field error={errorExists} warning={warningExists} classes="text-center" defaultValue={thirdDigit} onInput={(e: any) => handleDigitInput(e, 2)} onPaste={handlePaste} />
-                <Field error={errorExists} warning={warningExists} classes="text-center" defaultValue={fourthDigit} onInput={(e: any) => handleDigitInput(e, 3)} onPaste={handlePaste} />
-                <Field error={errorExists} warning={warningExists} classes="text-center" defaultValue={fifthDigit} onInput={(e: any) => handleDigitInput(e, 4)} onPaste={handlePaste} />
-                <Field error={errorExists} warning={warningExists} classes="text-center" defaultValue={sixthDigit} onInput={(e: any) => handleDigitInput(e, 5)} onPaste={handlePaste} />
-            </div>
+                <strong className="block font-semibold text-lg text-center mt-2 select-none">Verify Your Account</strong>
+                <div className="text-sm font-medium text-center text-slate-400 select-none mb-7 dark:text-zinc-400">Enter the code sent to your email</div>
 
-            <Button classes="block w-full mt-2.5" loading={loading} disabled={errorExists || warningExists}>Verify</Button>
-        </form>
+                <VerificationForm email={email} />
+            </section>
+        </main>
     );
 }
