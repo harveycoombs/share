4f60908db1d21f3d916ede13a4f5bdcf05a8204b
Harvey Coombs <harveycoombs23@outlook.com>
2025-07-22 16:59:21 +0100

    Added animation to popup background

--- app/components/common/Popup.tsx
+++ app/components/common/Popup.tsx
@@ -13,7 +13,13 @@ interface Properties {
 
 export default function Popup({ title, onClose, children, classes, ...rest }: Properties) {
     return (
-        <div className="fixed inset-0 z-50 grid place-items-center bg-slate-900/60 backdrop-blur-xs" id="popup" onMouseDown={(e: any) => {if (e.target.matches("#popup")) onClose() }}>
+        <motion.div 
+            initial={{ backgroundColor: "color-mix(in oklab,var(--color-slate-900)0%,transparent)", backdropFilter: "blur(0px)" }}
+            animate={{ backgroundColor: "color-mix(in oklab,var(--color-slate-900)60%,transparent)", backdropFilter: "blur(4px)" }}
+            transition={{ duration: 0.15, ease: "easeOut" }}
+            className="fixed inset-0 z-50 grid place-items-center" id="popup" 
+            onMouseDown={(e: any) => {if (e.target.matches("#popup")) onClose() }}
+        >
             <motion.div initial={{ scale: 0, opacity: 0 }} animate={{ scale: 1, opacity: 1, transition: { duration: 0.15, ease: "easeOut" }}} className={`bg-white p-3 rounded-lg${classes?.length ? " " + classes : ""}`} {...rest}>
                 <div className="flex justify-between items-center leading-none pb-1.5">
                     <strong className="text-sm font-medium text-slate-400/60 select-none">{title}</strong>
@@ -36,6 +42,6 @@ export default function Popup({ title, onClose, children, classes, ...rest }: Pr
                     {children}
                 </div>
             </motion.div>
-        </div>
+        </motion.div>
     );
 }
