66d2a2f1381743710709371b5418882f5272acb2
Harvey Coombs <harveycoombs23@outlook.com>
2025-07-22 16:35:07 +0100

    Moved Ad credentials to envrionment variables

--- app/components/common/Advertisement.tsx
+++ app/components/common/Advertisement.tsx
@@ -4,10 +4,10 @@ import { useEffect } from "react";
 export default function Advertisement() {
     useEffect(() => {
         (window as any).atOptions = {
-            key : "d1be5247516a9c83605c1b02ccd06b64",
+            key : process.env.NEXT_PUBLIC_ADSTERRA_KEY ?? "",
             format : "iframe",
-            height : 50,
-            width : 320,
+            width : parseInt(process.env.NEXT_PUBLIC_ADSTERRA_WIDTH ?? "0"),
+            height : parseInt(process.env.NEXT_PUBLIC_ADSTERRA_HEIGHT ?? "0"),
             params : {}
         };
 
@@ -26,5 +26,5 @@ export default function Advertisement() {
         }
     }, []);
 
-    return <div id="adv" style={{ width: 320, height: 50 }}></div>;
+    return <div id="adv" style={{ width: parseInt(process.env.NEXT_PUBLIC_ADSTERRA_WIDTH ?? "0"), height: parseInt(process.env.NEXT_PUBLIC_ADSTERRA_HEIGHT ?? "0") }}></div>;
 }
