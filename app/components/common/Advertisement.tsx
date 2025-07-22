8fef252ae769c2b6af65eb49388b93c1b83bf71a
Harvey Coombs <harveycoombs23@outlook.com>
2025-07-22 11:32:42 +0100

    Ad size change

--- app/components/common/Advertisement.tsx
+++ app/components/common/Advertisement.tsx
@@ -4,17 +4,17 @@ import { useEffect } from "react";
 export default function Advertisement() {
     useEffect(() => {
         (window as any).atOptions = {
-            key: "e764adcc0b5e6389c4608bb865f0b9c5",
-            format: "iframe",
-            height: 250,
-            width: 300,
-            params: {}
+            key : "8ebd9c6e4a6ef00a29109f43bd5d2c32",
+            format : "iframe",
+            height : 60,
+            width : 468,
+            params : {}
         };
 
         const script = document.createElement("script");
 
         script.type = "text/javascript";
-        script.src = "https://www.highperformanceformat.com/e764adcc0b5e6389c4608bb865f0b9c5/invoke.js";
+        script.src = "https://www.highperformanceformat.com/8ebd9c6e4a6ef00a29109f43bd5d2c32/invoke.js";
         script.async = true;
 
         const container = document.querySelector("#adv");
@@ -26,5 +26,5 @@ export default function Advertisement() {
         }
     }, []);
 
-    return <div id="adv" style={{ width: 300, height: 250 }}></div>;
+    return <div id="adv" style={{ width: 468, height: 60 }}></div>;
 }
