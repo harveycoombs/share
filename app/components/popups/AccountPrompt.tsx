b1357f71484a37a989691fa9b67f5dc0d83b4b3e
Harvey Coombs <70911622+harveycoombs@users.noreply.github.com>
2025-08-13 15:47:13 +0100

    Spacing adjustment + Updated version number

--- app/components/popups/AccountPrompt.tsx
+++ app/components/popups/AccountPrompt.tsx
@@ -9,9 +9,9 @@ export default function AccountPrompt({ onClose }: Properties) {
     return (
         <Popup title="Share Account" onClose={onClose}>
             <div className="w-80">
-                <strong className="block text-center text-lg font-semibold">Get More out of Share</strong>
+                <strong className="block text-center text-lg font-semibold mt-1">Get More out of Share</strong>
 
-                <div className="font-medium text-sm text-center text-slate-400 mt-1 mb-10">Log in or create an account to access<br/>more features</div>
+                <div className="font-medium text-sm text-center text-slate-400 mt-1 mb-5">Log in or create an account to access<br/>more features</div>
 
                 <Button url="/login" classes="block w-full">Sign In</Button>
                 <Button url="/register" classes="block w-full mt-2" color="gray">Sign Up</Button>
