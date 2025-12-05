db4e96fe13a4e2f6fd158e5463b873d577f4e552
harveycoombs <harveycoombs23@outlook.com>
2025-12-05 23:05:24 +0000

    Moved view counter increase

--- proxy.ts
+++ proxy.ts
@@ -12,14 +12,14 @@ export async function proxy(request: NextRequest) {
     const id = pathname.slice(9);
     const password = request.headers.get("Share-Upload-Password");
 
+    await incrementUploadViews(id);
+
     const passwordSet = await checkPasswordIsSet(id);
 
     if (!password && passwordSet) return NextResponse.redirect(`https://share.surf/protected/${id}`);
 
     const valid = await verifyUploadPassword(id, password ?? "");
     if (!valid) return NextResponse.json({ error: "Invalid password." }, { status: 401 });
-
-    await incrementUploadViews(id);
 
     return NextResponse.redirect(`https://uploads.share.surf/share/uploads/${id}`);
 }
