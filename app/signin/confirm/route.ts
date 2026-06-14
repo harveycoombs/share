e1ba4f42b6c346c070693a0e333767409c6ab8e5
Harvey Coombs <70911622+harveycoombs@users.noreply.github.com>
2026-06-14 23:27:18 +0100

    sign in error link correction

--- app/signin/confirm/route.ts
+++ app/signin/confirm/route.ts
@@ -13,11 +13,11 @@ export async function GET(request: Request): Promise<NextResponse> {
 
     const user = await getUserByEmailAddress(email);
     
-    if (!user) return NextResponse.redirect(new URL("/signin/confirm/error", request.url));
+    if (!user) return NextResponse.redirect(new URL("/signin/error", request.url));
 
     const valid = await verifyUserAccessCode(email, code);
 
-    if (!valid) return NextResponse.redirect(new URL("/signin/confirm/error", request.url));
+    if (!valid) return NextResponse.redirect(new URL("/signin/error", request.url));
 
     await updateUserAccessDate(email);
     await updateUserAccessCode(email, null);
