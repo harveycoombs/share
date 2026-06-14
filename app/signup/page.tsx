2b4bf9964e9e357aff5013165e43bc37159b3be2
Harvey Coombs <70911622+harveycoombs@users.noreply.github.com>
2026-06-14 23:13:37 +0100

    query string fix

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,13 +1,19 @@
 "use client";
 import Link from "next/link";
-import { useSearchParams } from "next/navigation";
+import { useState, useEffect } from "react";
 
 import Logo from "@/app/components/common/Logo";
 import RegistrationForm from "@/app/signup/form";
 
 export default function Register() {
-    const params = useSearchParams();
-    const email = params.get("email") ?? "";
+    const [email, setEmail] = useState<string>("");
+
+    useEffect(() => {
+        const params = new URLSearchParams(window.location.search);
+        const email = params.get("email") ?? "";
+
+        setEmail(email);
+    });
 
     return (
         <main className="min-h-[calc(100vh-101px)] grid place-items-center">
@@ -16,8 +22,6 @@ export default function Register() {
 
                 <strong className="block font-semibold text-lg text-center mt-2 select-none">Get More out of Share</strong>
                 <div className="text-sm font-medium text-center text-slate-400 select-none mb-7">Sign up using the form below</div>
-
-                params: {JSON.stringify(params)}
 
                 email: {email}<br/>
 
